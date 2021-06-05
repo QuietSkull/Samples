671c6d1f283b8d51515088fd004e5a6885a39421
QuietSkull <68357531+QuietSkull@users.noreply.github.com>
2021-06-05 15:28:42 -0400

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,4 +1,4 @@
-# This is a simple calculater program to add, subtract, multiply or divide numbers.
+## This is a simple calculater program to add, subtract, multiply or divide numbers.
 # This  adds two numbers
 def add(x, y):
     return x + y
@@ -13,29 +13,28 @@ def divide(x, y):
     return x / y
 # Player's options for input.
 print("Select operation.")
-print("Add")
-print("Subtract")
-print("Multiply")
-print("Divide")
+print("(1) Add")
+print("(2) Subtract")
+print("(3) Multiply")
+print("(4) Divide")
 while True:
-# Take input from the user
-    choice = input("Enter choice:")
-# Check if choice is one of the four options
-    if choice in ('Add', 'Subtract', 'Multiply', 'Divide'):
-        num1 = int(input("Enter first number: "))
-        num2 = int(input("Enter second number: "))
+    choice = input("Enter choice:") # Take input from the user
+# The players pivks which one of the four options
+    if choice in ('1', '2', '3', '4'):
+        num1 = int(input("Enter first number: ")) # Take first input integer num from the player
+        num2 = int(input("Enter second number: ")) # Take second input integer num from the player
 
-        if choice == 'Add':
-            print(num1, "+", num2, "=", add(num1, num2))
+        if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2)) # adding num1 and Num2 together
 
-        elif choice == 'Subtract':
-            print(num1, "-", num2, "=", subtract(num1, num2))
+        elif choice == '2':
+            print(num1, "-", num2, "=", subtract(num1, num2)) # subtract num1 and Num2
 
-        elif choice == 'Multiply':
-            print(num1, "*", num2, "=", multiply(num1, num2))
+        elif choice == '3':
+            print(num1, "*", num2, "=", multiply(num1, num2)) # multiply num1 and Num2 together
 
-        elif choice == 'Divide':
-            print(num1, "/", num2, "=", divide(num1, num2))
-        break
+        elif choice == '4':
+            print(num1, "/", num2, "=", divide(num1, num2)) # divide num1 and Num2 together
+        break # end program
     else:
         print("Invalid Input")
