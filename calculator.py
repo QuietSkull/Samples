69a982b665e2a548023fca47a71213205ac3897b
QuietSkull <68357531+QuietSkull@users.noreply.github.com>
2021-06-05 15:10:25 -0400

    added more comments

--- calculator.py
+++ calculator.py
@@ -1,17 +1,17 @@
-# Program make a simple calculator
-# This function adds two numbers
+# This is a simple calculater program to add, subtract, multiply or divide numbers. 
+# This  adds two numbers
 def add(x, y):
     return x + y
-# This function subtracts two numbers
+# This  subtracts two numbers
 def subtract(x, y):
     return x - y
-# This function multiplies two numbers
+# This  multiplies two numbers
 def multiply(x, y):
     return x * y
-# This function divides two numbers
+# This  divides two numbers
 def divide(x, y):
     return x / y
-# Player's options
+# Player's options for input.
 print("Select operation.")
 print("1.Add")
 print("2.Subtract")
@@ -19,7 +19,7 @@ print("3.Multiply")
 print("4.Divide")
 while True:
 # Take input from the user
-    choice = input("Enter choice(1/2/3/4):")
+    choice = input("Enter choice:")
 # Check if choice is one of the four options
     if choice in ('1', '2', '3', '4'):
         num1 = int(input("Enter first number: "))
